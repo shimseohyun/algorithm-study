80a225881b92c3841475734250512252bf234da0
shimseohyun <shim020924@gmail.com>
2025-01-16 17:11:12 +0900

    14888 - 연산자 끼워넣기 #34
    입력방식과, 함수형태를 바꿔서 8ms를 줄였어요... 하지만 더 최적화 할 수 있을듯

--- shim/250116/14888.js
+++ shim/250116/14888.js
@@ -1,42 +1,11 @@
-const input = require("fs")
+const [, A, sign] = require("fs")
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../input.txt")
   .toString()
   .trimEnd()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.trim().split(" ").map(Number));
 
-// 첫째 줄에 수의 개수 N(2 ≤ N ≤ 11)가 주어진다.
-const N = parseInt(input[0]);
-
-// 둘째 줄에는 A1, A2, ..., AN이 주어진다. (1 ≤ Ai ≤ 100)
-const A = input[1].split(" ").map(Number);
-
-// 차례대로 덧셈(+)의 개수, 뺄셈(-)의 개수, 곱셈(×)의 개수, 나눗셈(÷)의 개수
-const sign = input[2].split(" ").map(Number);
-const ADD = 0;
-const SUBTRACT = 1;
-const MULTIPLICATION = 2;
-const DIVISION = 3;
-
-const getResult = (s, result, num) => {
-  switch (s) {
-    case ADD:
-      return result + num;
-    case SUBTRACT:
-      return result - num;
-    case MULTIPLICATION:
-      return result * num;
-    case DIVISION:
-      // 나눗셈은 정수 나눗셈으로 몫만 취한다
-      // C++14의 기준을 따른다.
-      // 즉, 양수로 바꾼 뒤 몫을 취하고, 그 몫을 음수로 바꾼 것과 같다.
-
-      if (result < 0) {
-        return Math.ceil(result / num);
-      } else {
-        return Math.floor(result / num);
-      }
-  }
-};
+const N = A.length;
 
 const getSignsArr = () => {
   const result = [];
@@ -69,7 +38,28 @@ for (let signs of signsArr) {
   let result = A[0];
 
   for (let i = 0; i < N - 1; i++) {
-    result = getResult(signs[i], result, A[i + 1]);
+    switch (signs[i]) {
+      case 0:
+        result = result + A[i + 1];
+        break;
+      case 1:
+        result = result - A[i + 1];
+        break;
+      case 2:
+        result = result * A[i + 1];
+        break;
+      case 3:
+        // 나눗셈은 정수 나눗셈으로 몫만 취한다
+        // C++14의 기준을 따른다.
+        // 즉, 양수로 바꾼 뒤 몫을 취하고, 그 몫을 음수로 바꾼 것과 같다.
+
+        if (result < 0) {
+          result = Math.ceil(result / A[i + 1]);
+        } else {
+          result = Math.floor(result / A[i + 1]);
+        }
+        break;
+    }
   }
 
   if (result > max) {
