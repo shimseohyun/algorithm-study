46fbd67f512b06807f28aabd76005612a575347e
shyun2e <dhaprk0429@naver.com>
2025-01-21 10:20:18 +0900

    최소힙으로 개선

--- park/20250120/9370.js
+++ park/20250120/9370.js
@@ -7,6 +7,72 @@ const input = require("fs")
 const T = parseInt(input[0]);
 let index = 1;
 
+
+//최소 힙 구현
+class MinHeap{
+    constructor(){
+        this.heap = [];
+    }
+
+    size(){
+        return this.heap.length;
+    }
+
+    swap(idx1, idx2){
+        [this.heap[idx1] , this.heap[idx2]] =[this.heap[idx2] , this.heap[idx1]];
+    }
+
+    add(value){
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    poll(){
+        if(this.heap.length === 1)  return this.heap.pop();
+        const value = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return value;
+    }
+    bubbleUp(){
+        let index = 0;
+        let parentIdx = Math.floor((index-1) / 2);
+        while(this.heap[parentIdx] && this.heap[index][1] < this.heap[parentIdx][1]){
+            this.swap(index, parentIdx);
+            index = parentIdx;
+            parentIdx = Math.floor( (index-1) / 2);
+        }
+    }
+
+    bubbleDown() {
+        let index = 0;
+        let leftIdx = index * 2 + 1;
+        let rightIdx = index * 2 + 2;
+    
+        while (
+          (this.heap[leftIdx] && this.heap[leftIdx][1] < this.heap[index][1]) ||
+          (this.heap[rightIdx] && this.heap[rightIdx][1] < this.heap[index][1])
+        ) {
+          let smallerIdx = leftIdx;
+          if (
+            this.heap[rightIdx] &&
+            this.heap[rightIdx][1] < this.heap[smallerIdx][1]
+          ) {
+            smallerIdx = rightIdx;
+          }
+    
+          this.swap(index, smallerIdx);
+          index = smallerIdx;
+          leftIdx = index * 2 + 1;
+          rightIdx = index * 2 + 2;
+        }
+      }
+    
+}
+
+
+
+
 for (let i = 0; i < T; i++) {
   const [n, m, t] = input[index++].split(" ").map(Number);
   const [s, g, h] = input[index++].split(" ").map(Number);
@@ -26,22 +92,23 @@ for (let i = 0; i < T; i++) {
   }
 
   // 다익스트라 알고리즘(추가 공부 해야 할듯...)
-  function dijkstra(start) {
+function dijkstra(start) {
     const dist = Array(n + 1).fill(Infinity);
-    const queue = [[start, 0]];
+    const minHeap = new MinHeap();
+    minHeap.add([start,0]);
     dist[start] = 0;
 
-    while (queue.length > 0) {
-      const [curNode, curDist] = queue.shift();
+    while (minHeap.size() > 0) {
+      const [curNode, curDist] = minHeap.poll();
 
       if (curDist > dist[curNode]) continue;
-
+        //현재 노드와 인접한 다음 노드 및 가중치 for문으로 계산
       for (const [nextNode, weight] of graph[curNode]) {
         const nextDist = curDist + weight;
 
         if (nextDist < dist[nextNode]) {
           dist[nextNode] = nextDist;
-          queue.push([nextNode, nextDist]);
+          minHeap.add([nextNode, nextDist]);
         }
       }
     }
